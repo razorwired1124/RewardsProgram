8226ea8d64b1d5a4f4bb9578974d179f913e647a
Jacob McCullough <razorwired1124@gmail.com>
2019-05-24 03:41:25 -0500

    Added Code Snippets for README
    I added several code snippets to put in my readme and explain what I did.

--- rewards_program/Rewards_Program.py
+++ rewards_program/Rewards_Program.py
@@ -115,19 +115,3 @@ for item in purchases:
         else:
             print('0')
         print("Total Rewards: ", 300 + 58 + 0 + 42)
-
-
-
-purchase = input("How much was your purchase? : ")
-over_50 = int(purchase) - 50
-over_100 = int(purchase) - 100 + over_50
-
-print("")
-
-if int(purchase) > 50 and int(purchase) <= 100:
-    print("You have gained " + str(over_50) + " rewards points with your purchase")
-elif int(purchase) > 100:
-    print("You have gained " + str(over_100) + " rewards points with your purchase")
-else:
-    print("You have gained 0 rewards points")
-    
